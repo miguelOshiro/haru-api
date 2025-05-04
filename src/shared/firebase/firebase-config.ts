e6a31a2e30c01b61738cf7ec9f330ce9c70a7235
miguel oshiro <miguel@miguels-Mac-Studio.local>
2025-05-03 21:21:52 -0500

    login and register

--- src/shared/firebase/firebase-config.ts
+++ src/shared/firebase/firebase-config.ts
@@ -8,7 +8,12 @@ import { FirebaseStorage, getStorage } from 'firebase/storage';
 
 
 const firebaseConfig = {
-  
+  apiKey: "AIzaSyA-AoCdgxDRVWtLJg-X6h9kS-KFr9dDrSI",
+  authDomain: "haru-77a68.firebaseapp.com",
+  projectId: "haru-77a68",
+  storageBucket: "haru-77a68.firebasestorage.app",
+  messagingSenderId: "446850140059",
+  appId: "1:446850140059:web:65f52bda4467932296433e"
 };
 
 const app = initializeApp(firebaseConfig);
